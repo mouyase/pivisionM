794edc1c77d043f07df4b690bfcb53d8b7079f27
某亚瑟 <mouyase@qq.com>
2019-10-24 20:22:35 +0800

    v4.4.3(443003) 1. 紧急修复无法登陆的问题

---
+++ app/src/main/java/tech/yojigen/common/MD5.java
@@ -0,0 +1,25 @@
+package tech.yojigen.common;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+
+    public static String convert(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10)
+                hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+}
